c1ee99ff878b2df4aef590046415861a00c9f4b6
Abdulmounem Alhamwi <abod.h95@gmail.com>
2022-02-13 11:37:04 +0400

    Fixed npm package file

--- GoogleMapLocation/generated/ManifestTypes.d.ts
+++ GoogleMapLocation/generated/ManifestTypes.d.ts
@@ -4,24 +4,24 @@
 
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
+    Latitude: ComponentFramework.PropertyTypes.NumberProperty;
+    Longitude: ComponentFramework.PropertyTypes.NumberProperty;
     AddressName: ComponentFramework.PropertyTypes.StringProperty;
     Country: ComponentFramework.PropertyTypes.StringProperty;
     City: ComponentFramework.PropertyTypes.StringProperty;
     Street: ComponentFramework.PropertyTypes.StringProperty;
     Telephone: ComponentFramework.PropertyTypes.StringProperty;
     PostalCode: ComponentFramework.PropertyTypes.StringProperty;
-    Latitude: ComponentFramework.PropertyTypes.NumberProperty;
-    Longitude: ComponentFramework.PropertyTypes.NumberProperty;
     GoogleAPIKey: ComponentFramework.PropertyTypes.StringProperty;
     ButtonColor: ComponentFramework.PropertyTypes.StringProperty;
 }
 export interface IOutputs {
+    Latitude?: number;
+    Longitude?: number;
     AddressName?: string;
     Country?: string;
     City?: string;
     Street?: string;
     Telephone?: string;
     PostalCode?: string;
-    Latitude?: number;
-    Longitude?: number;
 }
